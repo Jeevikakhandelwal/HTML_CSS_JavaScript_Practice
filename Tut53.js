732d8998ae6c1b3d7326f34f4492225684ac4656
unknown <jeevikakhandelwal07@gmail.com>
2023-04-09 22:55:39 +0530

    Initial commit

---
+++ Tut53.js
@@ -0,0 +1,21 @@
+// function greet(name1){
+//     console.log(name1+" is a good girl")
+// }
+function greet(name, greetText){
+    console.log(greetText+" "+name)
+    console.log(name+" is a good girl");
+}
+
+let name='jeevika';
+let name1='shanvi';
+let name2='chetana';
+let name3='chameli';
+let greetText ="Good Morning"
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+// console.log(name+" is a good girl");
+// console.log(name1+" is a good girl");
+// console.log(name2+" is a good girl");
+// console.log(name3+" is a good girl");
